9780429796f3889e5a0441af14bae46cad2b1fff
zytoabc <zytoabc@gmail.com>
2025-06-11 09:06:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
+      <h1>Welcome to abc-zyto-scanning</h1>
+      <p>This project is ready to deploy on Vercel and GitHub.</p>
+    </div>
+  );
+}
