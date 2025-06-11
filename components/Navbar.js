56040ab762d972034985a1e5ede00267745eff23
zytoabc <zytoabc@gmail.com>
2025-06-11 09:50:36 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.css';
 
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">ABC Zyto</Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/scan">Scan</Link></li>
+        <li><Link href="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+    }
