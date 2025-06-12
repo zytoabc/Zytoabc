d69bb903fbf5fd673d513e68f04b3b41ba7583f3
zytoabc <zytoabc@gmail.com>
2025-06-12 14:25:26 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.css';
-import { Menu, X } from 'lucide-react';
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <Link href="/">Zyto Balance</Link>
       </div>
-
-      <div className={styles.menuToggle} onClick={toggleMenu}>
-        {menuOpen ? <X size={24} /> : <Menu size={24} />}
-      </div>
-
-      <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ''}`}>
-        <li>
-          <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
-        </li>
-        <li>
-          <Link href="/scan" onClick={() => setMenuOpen(false)}>Scan</Link>
-        </li>
-        <li>
-          <Link href="/about" onClick={() => setMenuOpen(false)}>About</Link>
-        </li>
+      <ul className={styles.navLinks}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/scan">Scan</Link></li>
+        <li><Link href="/about">About</Link></li>
       </ul>
     </nav>
   );
