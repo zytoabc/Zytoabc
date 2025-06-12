10053b1f6730469e8375a7bad61c76222d6aa65d
zytoabc <zytoabc@gmail.com>
2025-06-12 14:53:15 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,12 +4,8 @@ import styles from '../styles/Home.module.css';
 export default function Navbar({ darkMode, toggleDarkMode }) {
   return (
     <div className={`${styles.navbar} ${darkMode ? styles.darkNavbar : styles.lightNavbar}`}>
-      <h2>ABC ZYTO Scanning</h2>
+      <h2>ZYTO Balance Scanning</h2>
       <div>
-        <a href="#about" className={styles.link}>About</a>
-        <a href="#coherence" className={styles.link}>Coherence</a>
-        <a href="#emotions" className={styles.link}>Emotions</a>
-        <a href="#gallery" className={styles.link}>Gallery</a>
         <button className={styles.button} onClick={toggleDarkMode}>
           {darkMode ? '🌙' : '☀️'}
         </button>
