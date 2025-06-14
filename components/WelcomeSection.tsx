2954af7c5576d9bb51675791e9e90a38f34bbb50
zytoabc <zytoabc@gmail.com>
2025-06-15 05:10:30 +0800

    Create WelcomeSection.tsx

---
+++ components/WelcomeSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function WelcomeSection() {
+  return (
+    <div>
+      <h1>🌈 Welcome to ZYTO Balance Scanning</h1>
+      <p>
+        Empowering personalized wellness through bio-communication technology.
+      </p>
+    </div>
+  );
+}
