a410d21742dda9eccfadaeac65c8fb502bef9643
zytoabc <zytoabc@gmail.com>
2025-06-16 09:52:01 +0800

    Update OverviewVideo.tsx

--- components/OverviewVideo.tsx
+++ components/OverviewVideo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function OverviewVideo() {
   return (
     <div style={{ marginTop: 40, maxWidth: 900 }}>
-      <h2>🎥 ZYTO Balance Overview Video</h2>
+      <h2>🎥 ZYTO ScanPH Overview Video</h2>
       <div
         style={{
           position: 'relative',
@@ -17,7 +17,7 @@ export default function OverviewVideo() {
       >
         <iframe
           src="https://www.youtube.com/embed/dFNnGsnMZxc"
-          title="ZYTO Balance Overview"
+          title="ZYTO ScanPH Overview"
           style={{
             position: 'absolute',
             top: 0,
